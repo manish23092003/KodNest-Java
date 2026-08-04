130ac2c2730146deb06838e9479cbee48db5a675
manish23092003 <manishsoni135790@gmail.com>
2026-08-04 12:31:13 +0530

    practise2

--- track/M02-T01-PP001/Practise2.java
+++ track/M02-T01-PP001/Practise2.java
@@ -2,7 +2,7 @@
 class Practise2 {
 
     public static void main(String[] args) {
-        int num = 25;
+        int num = 0;
         if (num > 0) {
             System.out.println("Positive");
         } else if (num < 0) {
