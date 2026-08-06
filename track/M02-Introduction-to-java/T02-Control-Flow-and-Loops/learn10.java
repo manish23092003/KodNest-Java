87aa0db4c82277b81a56ee9dd080bc0487c60fec
manish23092003 <manishsoni135790@gmail.com>
2026-08-06 12:43:16 +0530

    update

---
+++ track/M02-Introduction-to-java/T02-Control-Flow-and-Loops/learn10.java
@@ -0,0 +1,13 @@
+public class learn10{
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=2;j++ ){
+                if(j==2){
+                    break;
+                }
+                System.out.println("i value: " + i +"  j value: " + j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ track/M02-Introduction-to-java/T02-Control-Flow-and-Loops/learn11.java
@@ -0,0 +1,13 @@
+public class learn11{
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=4;j++ ){
+                if(j==2){
+                    continue;
+                }
+                System.out.println("i value: " + i +"  j value: " + j);
+            }
+            System.out.println();
+        }
+    }
+}
