5812fe666af386c505105f6b4c00906a1e118602
manish23092003 <manishsoni135790@gmail.com>
2026-08-07 11:22:02 +0530

    update

---
+++ track/M02-Introduction-to-java/T02-Control-Flow-and-Loops/learn13.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class learn13{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the byte value: ");
+        byte a = scan.nextByte();
+        System.out.println("byte value is: "+ a);
+
+        System.out.println("Enter the short value: ");
+        short b = scan.nextShort();
+        System.out.println("short value is: "+ b);
+
+        System.out.println("Enter the integer value: ");
+        int c = scan.nextInt();
+        System.out.println("integer value is: "+ c);
+
+        System.out.println("Enter the long value: ");
+        long d = scan.nextLong();
+        System.out.println("long value is: "+ d);
+
+        System.out.println("Enter the float value: ");
+        float e = scan.nextFloat();
+        System.out.println("float value is: "+ e);
+
+        System.out.println("Enter the double value: ");
+        double f = scan.nextDouble();
+        System.out.println("Double value is: "+ f);
+
+        System.out.println("Enter the boolean value: ");
+        boolean g = scan.nextBoolean();
+        System.out.println("Boolean value is: "+ g);
+
+
+    }
+}
