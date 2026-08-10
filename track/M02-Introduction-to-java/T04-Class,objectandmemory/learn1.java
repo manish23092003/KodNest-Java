5f2a45b18a2f6915baf12ac1dbf7a02953ff2ade
manish23092003 <manishsoni135790@gmail.com>
2026-08-10 13:05:08 +0530

    class

---
+++ track/M02-Introduction-to-java/T04-Class,objectandmemory/learn1.java
@@ -0,0 +1,27 @@
+class Student{
+    int roll;
+    String name;
+    double height;
+    void run()
+    {
+        System.out.println("runing");
+    }
+    void sleep()
+    {
+        System.out.println("Sleeping");
+    }
+}
+class learn1
+{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.roll = 15;
+        s1.name = "Manish";
+        s1.height = 5.5;
+        System.out.println(s1.roll);
+        System.out.println(s1.name);
+        System.out.println(s1.height);
+        s1.run();
+        s1.sleep();
+    }
+}
