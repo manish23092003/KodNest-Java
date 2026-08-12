63e8a7287ae998cfa3aee808a7acc72bde2ce519
manish23092003 <manishsoni135790@gmail.com>
2026-08-12 12:56:19 +0530

    update

---
+++ track/M02-Introduction-to-java/T05-Method,Shelding,overloading/learn2.java
@@ -0,0 +1,32 @@
+class Addition{
+    int add(int a, int b){
+        return a+b;
+    }
+    double add(int a, double b){
+        return a+b;
+    }
+    double add(double a,int b){
+        return a+b;
+    }
+    double add(double a,double b){
+        return a+b;
+    }
+    int add(int a,int b,int c){
+        return a+b+c;
+    }
+    double add(int a, int b, double c){
+        return a+b+c;
+    }
+    double add(int a,double b,double c){
+        return a+b+c;
+    }
+}
+class learn2{
+    public static void main(String[] args) {
+        Addition a1 = new Addition();
+        System.out.println(a1.add(2,7));
+        System.out.println(a1.add(10,20.5));
+
+    }
+
+}
