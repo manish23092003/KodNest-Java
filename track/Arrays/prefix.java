3a14467f2ff69407c08e1f6e613ce588f66d13dc
manish23092003 <manishsoni135790@gmail.com>
2026-09-03 21:35:01 +0530

    prefix

---
+++ track/Arrays/prefix.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class prefix{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        long prefix[] = new long[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(prefix[i] + " ");
+        }
+    }
+}
