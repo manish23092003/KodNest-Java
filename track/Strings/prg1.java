cbb22630ede43e06bfae8b2fcf2221d9ba2ce82c
manish23092003 <manishsoni135790@gmail.com>
2026-09-07 22:52:24 +0530

    String travesel

---
+++ track/Strings/prg1.java
@@ -0,0 +1,11 @@
+public class prg1{
+    public static void main(String[] args){
+        String s1 = "java";
+        String s2 = "java";
+        if(s1==s2){
+            System.out.println("Ref are same");
+        }else{
+            System.out.println("Ref are not same");   
+        }
+    }
+}
---
+++ track/Strings/prg4.java
@@ -0,0 +1,11 @@
+public class prg4{
+    public static void main(String[] args){
+        String s3 = new String("java");
+        String s4 = new String("java");
+        if(s3.equals(s4)){
+            System.out.println("String are equal");
+        }else{
+            System.out.println("String are not equal");
+        }
+    }
+}
