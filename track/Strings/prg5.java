8c25844b56dae47970d70859c4569375fbe8ea1f
manish23092003 <manishsoni135790@gmail.com>
2026-09-08 16:49:52 +0530

    String pool concept

---
+++ track/Strings/prg5.java
@@ -0,0 +1,16 @@
+public class prg5{
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "JaVa";
+        if(s1==s2){
+            System.out.println("Ref are same");
+        }else{
+            System.out.println("Ref are not same");
+        }
+        if(s1.equalsIgnoreCase(s2)){
+            System.out.println("String are same");
+        }else{
+            System.out.println("String are not same");
+        }
+    }
+}
